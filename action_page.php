97ddcd8a68032441c27cef5a190b9a65321cd6bb
Olivia Munoz <oliviarmunoz@gmail.com>
2023-09-17 10:10:05 -0400

    f

--- action_page.php
+++ action_page.php
@@ -1,12 +1,13 @@
 <?php
  $image = $_POST["output"];
  $name = $_POST["fname"];
+ $price = $_POST["price"]
  $negotiate = $_POST["negotiate"];
- $description = $_POST["paragraph_text"];
+ $description = $_POST["description"];
  $categories = $_POST["product-tag"];
 
- $keys = array($image, $name, $negotiate, $description, $categories); //THIS IS WHERE YOU PUT THE FORM ELEMENTS ex: array('$fieldA','$fieldB',etc)
- $csv_line = array();
+ $keys = array($image, $name, $price, $negotiate, $description, $categories); //THIS IS WHERE YOU PUT THE FORM ELEMENTS ex: array('$fieldA','$fieldB',etc)
+ $csv_line = $keys;
  foreach($keys as $key){
      array_push($csv_line,'' . $_GET[$key]);
  }
